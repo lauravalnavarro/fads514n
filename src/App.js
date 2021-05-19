b258affe6ac67518e2301408564cbdda5ef83172
Laura <lvna.95@gmail.com>
2021-05-18 19:13:51 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map(element => <li key={element}>{element}</li>)}
         </ul>
       </div>
     );
